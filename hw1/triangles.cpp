b6d788224e1011dc60805508820f3fbef0027741
andrewsg <andrewsgolman@gmail.com>
2020-03-13 18:58:27 +0300

    make them move

--- hw1/triangles.cpp
+++ hw1/triangles.cpp
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <GL/glew.h>
 #include <glfw3.h>
+#include <iostream>
+
 GLFWwindow* window;
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 
 #include <common/shader.hpp>
@@ -58,16 +62,17 @@ int main(void) {
 	GLuint programID1 = LoadShaders( "SimpleVertexShader.vertexshader", "FirstTriangle.fragmentshader" );
 	GLuint programID2 = LoadShaders( "SimpleVertexShader.vertexshader", "SecondTriangle.fragmentshader" );
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID1 = glGetUniformLocation(programID1, "MVP");
+	GLuint MatrixID2 = glGetUniformLocation(programID2, "MVP");
 
 	static const GLfloat g_vertex_buffer_data[] = {
 		 0.5f,  0.5f, 0.0f,
 		-0.9f,  0.0f, 0.0f,
 		 0.0f, -0.9f, 0.0f,
-		 0.0f,  0.0f, 0.0f,
-		 0.0f,  0.9f, 0.0f,
-		 0.9f,  0.0f, 0.0f,
+		 0.0f,  0.0f, 0.5f,
+		 0.0f,  0.9f, 0.5f,
+		 0.9f,  0.0f, 0.5f,
 	};
 
 	GLuint vertexbuffer;
@@ -75,23 +80,56 @@ int main(void) {
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS);
+
+
+	// Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	// initial pos
+	vec3 Eye = glm::vec3(2, 2, 0);
+	vec3 Center = glm::vec3(0, 0, 0);
+	vec3 Up = glm::vec3(0, 1, 0);
+
+	float time = 0.f;
+
 	do {
+		time += 1.f;
 
 		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// set camera position
+		Eye = glm::vec3(2 * cos(time / 100), 2, 2 * sin(time / 100));
+		// Camera matrix
+		glm::mat4 View = glm::lookAt(Eye, Center, Up);
+		// Our ModelViewProjection : multiplication of our 3 matrices
+		glm::mat4 MVP = Projection * View * Model;
 
 		// 1st attribute buffer : vertices
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 		// attribute, size, type, ifnormalized, stride, array buffer offset
 
 		// Use shader
 		glUseProgram(programID2);
+		// Send our transformation to the currently bound shader,
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID2, 1, GL_FALSE, &MVP[0][0]);
 		// Draw the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0
 
 		glUseProgram(programID1);
+		glUniformMatrix4fv(MatrixID1, 1, GL_FALSE, &MVP[0][0]);
 		glDrawArrays(GL_TRIANGLES, 3, 3); // 3 indices starting at 3
 
 		glDisableVertexAttribArray(0);
@@ -103,6 +141,8 @@ int main(void) {
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
+	glDisable(GL_BLEND);
+	glDisable(GL_DEPTH_TEST);
 	// Cleanup VBO
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteVertexArrays(1, &VertexArrayID);
